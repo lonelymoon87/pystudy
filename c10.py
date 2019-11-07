49986cc871f339e3a08620718e09e0ac195eeac4
lonelymoon87 <qytony.tech@gmail.com>
2019-11-08 01:43:18 +0800

    update

--- c10.py
+++ c10.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*
+from countword import countword
 
 # 10-3
 # 10-4
+'''
 while True:
     name = input("Please enter your name:\n")
     if name == 'q':
@@ -19,3 +21,52 @@ while True:
         break
     with open("reason.txt","a") as reasons:
         reasons.write(reason+"\n")
+'''
+
+# 10-6
+# add 
+'''
+1. try是要执行的语句，并不是要测试的语句
+2. try失败不继续执行，后面的语句需要跟else
+'''
+
+'''
+while True:
+    num1 = input("num1= ")
+    try:
+        num1 = int(num1)
+    except ValueError:
+        print("Wrong type,Please input num")
+    else:
+        num2 = input("num2= ")
+        try:
+            num2=int(num2)
+        except ValueError:
+            print("Wrong type,Please input num")
+        else:
+            print("%d + %d = %d" %(num1,num2,num1+num2))
+
+
+try:
+    with open("dogs.txt") as f_obj:
+        dogs = f_obj.readlines()
+except FileNotFoundError:
+    pass
+else:
+    for dog in dogs:
+        print(dog.strip())
+
+
+    
+try:
+    with open("cats.txt") as f_obj:
+        dogs = f_obj.readlines()
+except FileNotFoundError:
+    print("File no found")
+else:
+    for dog in dogs:
+        print(dog.strip())
+'''
+
+
+countword("test1.txt","乔")
