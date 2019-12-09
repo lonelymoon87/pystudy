ccd1a066a946c1a2f7413a9085b807e68713aa9a
cqy3@meitu.com <cqy3@meitu.com>
2019-12-09 17:34:48 +0800

    update

--- c18/learning_log/settings.py
+++ c18/learning_log/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r2ufx4$3azhly0e737n32*g!r1hfmd@-j61zr9b40wcr1^t4z6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
